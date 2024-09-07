1b4bdc8ee2d7b995261a569ddbe536ff7afcd5be
Anton Saprykin <saprykinanton10@gmail.com>
2024-09-07 20:36:31 +0300

    Last

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Саприкін Антон\nгр. Пз-21-1/9");
+        Date nowdate = new Date();
+        System.out.println(nowdate);
     }
 }
